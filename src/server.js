060fc44bbe4e27610fe2daaa477693eecae2ad0b
M. Rasyid Ridha <mrasyidridha532@gmail.com>
2021-06-08 21:18:13 +0700

    tambah plugin user di server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
+// import dotenv dan jalanin konfigurasinya
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const songsService = new SongsService();
+  const userService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +26,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugins: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugins: users,
+      options: {
+        service: userService,
+        validator: UsersValidator,
+      },
+    },
+  ]),
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
