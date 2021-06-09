524649cddd2cefcebb47f66ace118bfffdb79e02
M. Rasyid Ridha <mrasyidridha532@gmail.com>
2021-06-09 13:36:06 +0700

    add playlist validator

--- migrations/1623166644034_create-table-playlists.js
+++ migrations/1623166644034_create-table-playlists.js
@@ -28,7 +28,7 @@ exports.up = (pgm) => {
     memberikan constraint foreign key pada
     owner terhadap kolom id dari tabel users
   */
-  pgm.addConstraint('playlists', 'fk_playlists.owner_users.id','FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlists', 'fk_playlists.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
---
+++ src/validator/playlists/index.js
@@ -0,0 +1,22 @@
+const {
+  PostPlaylistPayloadSchema,
+  PostSongPlaylistPayloadSchema,
+} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const PlaylistsValidator = {
+  validatePostPlaylistPayload: (payload) => {
+    const validationResult = PostPlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validatePostSongPlaylistPayload: (payload) => {
+    const validationResult = PostSongPlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsValidator;
