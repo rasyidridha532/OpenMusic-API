2df8bf419bd31e3b4d72d47b88f9a497059b5b39
M. Rasyid Ridha <mrasyidridha532@gmail.com>
2021-06-09 16:19:46 +0700

    add playlist service

---
+++ src/services/postgres/PlaylistsService.js
@@ -0,0 +1,78 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylist({ name, owner }) {
+    const id = `playlist-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO playlists VALUES ($1, $2, $3) RETURNING id',
+      values: [id, name, owner],
+    };
+
+    const { rows } = await this._pool.query(query);
+
+    if (!rows.length) {
+      throw new InvariantError('Playlist Gagal Ditambahkan!');
+    }
+
+    return rows[0];
+  }
+
+  async getPlaylistsByUserId(owner) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE owner = $1',
+      values: [owner],
+    };
+
+    const { rows } = await this._pool.query(query);
+
+    if (!rows.length) {
+      throw new InvariantError('User tidak valid!');
+    }
+  }
+
+  async deletePlaylistByUserId(id) {
+
+  }
+
+  async addSongtoPlaylist({ songId, userId, playlistId }) {
+
+  }
+
+  async getSonginPlaylist({ songId, userId, playlistId }) {
+
+  }
+
+  async deleteSonginPlaylist({ songId, userId, playlistId}) {
+
+  }
+
+  async verifyPlaylistOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE id = $1',
+      values: [id],
+    };
+
+    const { rows } = await this._pool.query(query);
+
+    if (!rows.length) {
+      throw new NotFoundError('Playlist tidak ditemukan!');
+    }
+
+    const playlist = rows[0];
+
+    if (playlist.owner !== owner) {
+      throw new AuthorizationError('Anda tidak berhak mengakses playlist ini');
+    }
+  }
+}
+
+module.exports = PlaylistsService;
